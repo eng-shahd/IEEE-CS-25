485929fb13e948a1ce678f56e672e2a9561a0612
Shahd Mahmoud <158595641+eng-shahd@users.noreply.github.com>
2024-12-08 23:26:38 +0200

    Update Alice, Bob and Chocolate.cpp

--- Rookies/task3/Alice, Bob and Chocolate.cpp
+++ Rookies/task3/Alice, Bob and Chocolate.cpp
@@ -2,36 +2,32 @@
 using namespace std;
 
 int main() {
-
     int n;
-    cin >> n;
+    cin >>  n;
     int t[n];
-
     for (int i = 0; i < n; ++i) {
-
-        cin >> t[I];
-
+        cin >> t[i];
     }
 
-    int i = 0, j = n - 1;
+    int i = 0, j =  n - 1;
     int a_time = 0, b_time = 0;
     int a_bars = 0, b_bars = 0;
 
     while (i <= j) {
 
         if (a_time <= b_time) {
-
             a_time += t[i];
             a_bars++;
             i++;
-
         } else {
             b_time += t[j];
+
             b_bars++;
             j--;
         }
+
     }
 
-    cout << a_bars << " " << b_bars << endl ;
+    cout << a_bars << " " << b_bars << endl;
     return 0;
 }
